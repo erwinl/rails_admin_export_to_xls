7f135b3494fd2261631c57c84aa64cfce7c2c7e2
Erwin Luwis <eluwis@gmail.com>
2021-01-11 17:38:28 +0700

    Bug fixes:
    1. Fix `export_fields_for` method which incorrectly compares :symbols and strings.
    
    2. Fix `to_xls` method to properly generate xls file.
    
    3. Formatting for generated xls file.

--- lib/rails_admin_export_to_xls/xls_converter.rb
+++ lib/rails_admin_export_to_xls/xls_converter.rb
@@ -5,6 +5,9 @@ module RailsAdminExportToXls
     UTF8_ENCODINGS = [nil, '', 'utf8', 'utf-8', 'unicode', 'UTF8', 'UTF-8', 'UNICODE', 'utf8mb4']
     TARGET_ENCODINGS = %w(UTF-8 UTF-16LE UTF-16BE UTF-32LE UTF-32BE UTF-7 ISO-8859-1 ISO-8859-15 IBM850 MacRoman Windows-1252 ISO-8859-3 IBM852 ISO-8859-2 Windows-1250 IBM855 ISO-8859-5 KOI8-R MacCyrillic Windows-1251 IBM866 GB2312 GBK GB18030 Big5 Big5-HKSCS EUC-TW EUC-JP ISO-2022-JP Shift_JIS EUC-KR)
     def initialize(objects = [], schema = {})
+      @fields = []
+      @associations = []
+
       return self if (@objects = objects).blank?
 
       @model = objects.dup.first.class
@@ -50,13 +53,78 @@ module RailsAdminExportToXls
       # if options[:encoding_to].present? && @encoding_to == Encoding::UTF_8
       #   xls_string = "\xEF\xBB\xBF#{xls_string}"
       # end
-      xls_string = StringIO.new
       book = ::Spreadsheet::Workbook.new
+
+      # Font
+      book_format = Spreadsheet::Format.new(
+        font: Spreadsheet::Font.new('Arial'),
+        size: 10,
+        vertical_align: :middle,
+        horizontal_align: :left,
+      )
+      book.add_format(book_format)
+
       sheet = book.create_worksheet
+      sheet.default_format = book_format
+
+      # Header
+      header_format = Spreadsheet::Format.new(
+        size: 10,
+        weight: :bold,
+        vertical_align: :middle,
+        horizontal_align: :left,
+        # pattern_bg_color: :grey,
+        # pattern: 0xfc000000
+      )
+      sheet.row(0).default_format = header_format
+      sheet.row(0).height = 20
       sheet.row(0).concat generate_xls_header
-      book.write('/home/eminter25391/test.xls')
-      book.write(xls_string)
-      [!options[:skip_header], @encoding_to.to_s, xls_string.string]
+
+      # Content
+      row = 1
+      method = @objects.respond_to?(:find_each) ? :find_each : :each
+      @objects.send(method) do |object|
+        sheet.row(row).concat generate_xls_row(object)
+        sheet.row(row).height = 20
+        row = row + 1
+      end
+
+      # Column width formatting
+      (0...sheet.column_count).each do |col_idx| 
+        column = sheet.column(col_idx) 
+        column.width = column.each_with_index.map do |cell, row|
+          chars = if cell.present? 
+            arr = cell.to_s.strip.split("\n")
+            count = arr.map do |comp|
+              comp.split('').count
+            end.max
+            count + 3 # buffer
+          else
+            1
+          end
+
+          ratio = sheet.row(row).format(col_idx).font.size / 10
+          (chars * ratio).round
+        end.max 
+      end
+      # Column height formatting
+      (0...sheet.row_count).each do |row_idx|
+        row = sheet.row(row_idx)
+        row.height = row.each_with_index.map do |cell, column|
+          multiplier = if cell.present?
+            cell.to_s.lines.count
+          else
+            1
+          end
+          calculated_height = (row.format(column).font.size + 4) * multiplier
+          [calculated_height, 20].max
+        end.max
+      end
+
+      buffer = StringIO.new
+      book.write(buffer)
+
+      [!options[:skip_header], @encoding_to.to_s, buffer.string]
     end
 
   private
@@ -66,20 +134,21 @@ module RailsAdminExportToXls
     end
 
     def export_fields_for(method, model_config = @model_config)
-      model_config.export.fields.select { |f| f.name == method }
+      names = model_config.export.fields.collect { |f| f.name }
+      model_config.export.fields.select { |f| f.name.to_s == method.to_s }
     end
 
-    def generate_xls_string(options)
-      generator_options = (options[:generator] || {}).symbolize_keys.delete_if { |_, value| value.blank? }
-      CSV.generate(generator_options) do |xls|
-        xls << generate_xls_header unless options[:skip_header]
-
-        method = @objects.respond_to?(:find_each) ? :find_each : :each
-        @objects.send(method) do |object|
-          xls << generate_xls_row(object)
-        end
-      end
-    end
+    # def generate_xls_string(options)
+    #   generator_options = (options[:generator] || {}).symbolize_keys.delete_if { |_, value| value.blank? }
+    #   CSV.generate(generator_options) do |xls|
+    #     xls << generate_xls_header unless options[:skip_header]
+    #
+    #     method = @objects.respond_to?(:find_each) ? :find_each : :each
+    #     @objects.send(method) do |object|
+    #       xls << generate_xls_row(object)
+    #     end
+    #   end
+    # end
 
     def generate_xls_header
       @fields.collect do |field|
